0fcfa6ae2f2d561793df18456802d8bfd2e2df99
Paul Leavitt <pleavitt@gmail.com>
2018-08-27 13:24:37 +1000

    Updates

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { searchReducer } from './reducer';
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-export default function configureStore() {
-  return createStoreWithMiddleware(searchReducer);
-}
